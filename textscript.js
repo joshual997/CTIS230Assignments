6b0476b9151811a6a304f6cd941799cd30efb2f6
joshual997 <jlong4@guilford.edu>
2025-03-11 09:54:31 -0400

    Update textscript.js

--- textscript.js
+++ textscript.js
@@ -1,8 +1,13 @@
 myitem = document.getElementById("firsttest");
 
 myitem.addEventListener("click", onClick);
+myitem.addEventListener("out", onmouseout);
 
 function onClick() {  
     myitem.style.color = "green";  
     myitem.style.fontSize = "50px";
+}
+function onmouseout() {
+    myitem.style.color = "";
+    myitem.style.fontSize = "";
 }
